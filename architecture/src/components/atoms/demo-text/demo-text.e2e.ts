c3610f8dd5d6b27d1aa24d36cad7b1b075446aca
Roy Surjano <rsurjano@outlook.com>
2020-06-01 17:00:16 -0500

    docs(6-3): 📝 6-3 final

---
+++ architecture/src/components/atoms/demo-text/demo-text.e2e.ts
@@ -0,0 +1,29 @@
+import { E2EElement, newE2EPage } from "@stencil/core/testing";
+import { DemoText } from "./demo-text";
+import { buildComponentTest } from "./demo-text.helpers";
+
+const component = buildComponentTest(newE2EPage, [DemoText]);
+
+describe("should test e2e demo-text component", () => {
+  let page: E2EElement;
+  let element: E2EElement;
+
+  describe("rendering", () => {
+    it("should be able to render", async () => {
+      page = await component({ lastname: "source" });
+      element = await page.find("demo-text");
+      expect(element).toBeTruthy();
+    });
+  });
+
+  describe("styles", () => {
+    it("should match styles for component", async () => {
+      page = await component({ lastname: "source" });
+      element = await page.find("demo-text");
+      const styles = {
+        fontSize: "18px",
+      };
+      expect(await element.getComputedStyle()).toMatchObject(styles);
+    });
+  });
+});
